b4d7c2480bbfd787a85ce068f63bda3dcdb561b0
Aarnow <arnaudfgr.dev@gmail.com>
2024-03-27 18:32:40 +0100

    add interactions & documents

---
+++ InteractionKitCooldown.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using UnityEngine;
+
+namespace InteractionKit
+{
+    public class InteractionKitCooldown : ModKit.ORM.ModEntity<InteractionKitCooldown>
+    {
+        [AutoIncrement][PrimaryKey] public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public long LastFrisked { get; set; }
+        public long LastSteal { get; set; }
+        public long LastKnockedOut { get; set; }
+        public long LastRestrain { get; set; }
+
+        public InteractionKitCooldown()
+        {
+        }
+    }
+}
