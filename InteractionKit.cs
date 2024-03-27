769b52a67f8739e9de1c08f5afea7e53d590454c
Aarnow <arnaudfgr.dev@gmail.com>
2024-03-27 12:21:27 +0100

    Initial Project Files

---
+++ InteractionKit.cs
@@ -0,0 +1,18 @@
+﻿using Life;
+using ModKit.Interfaces;
+
+namespace InteractionKit
+{
+    public class InteractionKit : ModKit.ModKit
+    {
+        public InteractionKit(IGameAPI api) : base(api)
+        {
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();          
+            ModKit.Internal.Logger.LogSuccess($"{PluginInformations.SourceName} v{PluginInformations.Version}", "initialisé");
+        }
+    }
+}
