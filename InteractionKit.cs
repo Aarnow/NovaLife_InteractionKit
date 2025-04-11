dac82090076bdeec985da567f84cf9ae1307c863
zerox-hue <wakzzedit2@gmail.com>
2025-04-11 20:04:50 +0200

    Correctif condition

--- InteractionKit.cs
+++ InteractionKit.cs
@@ -153,8 +153,11 @@ namespace InteractionKit
                 {
                     var target = player.GetClosestPlayer();
 
-                    if (target != null) ToBeKnockedOut(target, player);
-                    else if (target.Health <= 0) { player.Notify("Échec", "La cible est morte"); }
+                    if (target != null)
+                    {
+                        if (target.Health > 0) ToBeKnockedOut(target, player);
+                        else player.Notify("Échec", "Le citoyen est déjà dans le coma", NotificationManager.Type.Error);
+                    }
                     else player.Notify("Échec", "Aucun citoyen à proximité", NotificationManager.Type.Error);
                 }
             });
